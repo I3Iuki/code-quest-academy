8b5e4727e293d7bc69b0296110971de1c49e51ac
I3Iuki <nguyenkien20101011@gmail.com>
2026-08-08 18:27:45 -0400

    Solved Bigger is Better

--- cpp/bigger-is-better/main.cpp
+++ cpp/bigger-is-better/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
     int cases;
-    std::cin >> cases;
+    cin >> cases;
+    cin.ignore();
 
     for (int _ = 0; _ < cases; ++_) {
+        vector<int> arr;
         
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        
+        int temp;
+
+        while (ss >> temp) {
+            arr.push_back(temp);
+        }
+
+        cout << *max_element(arr.begin(), arr.end()) << '\n';
     }
 }
