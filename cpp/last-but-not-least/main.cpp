ceb646925abdf42ac4a0f56240f716d79ec9d083
I3Iuki <nguyenkien20101011@gmail.com>
2026-09-04 11:14:04 -0400

    Resolvedd merge conflicts with last-but-not-least

--- cpp/last-but-not-least/main.cpp
+++ cpp/last-but-not-least/main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int cases = 0;
     cin >> cases;
     cin.ignore(1000, '\n');
-    for (int _ = 0; _ < cases; ++_) {
+    for (int _ = 0; _ < cases; ++_)
+    {
 
         string s;
         getline(cin, s);
 
-        if (s[s.size() - 2] == ' ' || s[s.size() - 2] == '"')
+        if (isalpha(static_cast<unsigned char>(s[s.size() - 2])))
+        {
+            cout << s[s.size() - 2] << '\n';
+        }
+        else
         {
             cout << "No Letter Found\n";
-        } else {
-            cout << s[s.size() - 2] << '\n';
         }
     }
 }
